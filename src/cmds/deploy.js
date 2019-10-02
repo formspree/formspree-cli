4cc1eb55c3034b0eff77e896a49e8b7b66025802
Derrick Reimer <derrickreimer@gmail.com>
2019-10-02 12:03:59 -0500

    Add config parsing

--- src/cmds/deploy.js
+++ src/cmds/deploy.js
@@ -1,5 +1,45 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+const readRawConfigSync = () => {
+  try {
+    return fs.readFileSync('statickit.json', 'utf8');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return null;
+    } else {
+      throw err;
+    }
+  }
+};
+
+const parseRawConfig = rawConfig => {
+  try {
+    const json = JSON.parse(rawConfig);
+    return json;
+  } catch (err) {
+    return null;
+  }
+};
+
 exports.builder = {};
 
-exports.handler = args => {
-  console.log('TODO: Deploy');
+exports.handler = _args => {
+  const rawConfig = readRawConfigSync();
+
+  if (!rawConfig) {
+    console.error(chalk.bold.red('statickit.json does not exist'));
+    process.exitCode = 1;
+    return;
+  }
+
+  const config = parseRawConfig(rawConfig);
+
+  if (!config) {
+    console.error(chalk.bold.red('statickit.json could not be parsed'));
+    process.exitCode = 1;
+    return;
+  }
+
+  console.log(`Config: ${rawConfig}`);
 };
